1f133b2a5660e20df22455c6d7480783f8bb9222
mbathe <mbathepaul@gmail.com>
2024-10-10 10:56:33 +0200

    Téléchargement du dataset à partir de google driveé

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv
+from tests import load_dataset_test
 from src.utils.streamlit import st
 from src.zone_sacha.app import *
+from dotenv import load_dotenv
 load_dotenv()
 
 
+
 if __name__ == "__main__":
-    print('test')
+    print('APPLICATION STARTED...')
