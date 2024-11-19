3ef2c90082680b22e4418b7412e264f7c2b7da73
AlexMovsess <73705463+AlexMovsess@users.noreply.github.com>
2024-11-19 10:45:08 +0100

    added nutrition page

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 from tests import load_dataset_test
 from src.utils.streamlit import st
-from src.zone_sacha.app import *
+# from src.zone_sacha.app import *
 from dotenv import load_dotenv
 load_dotenv()
 
-
-
 if __name__ == "__main__":
     print('APPLICATION STARTED...')
-    
